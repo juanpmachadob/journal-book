ab133ad7203955d319ae388554da5d54b42832dd
Ju4nPx <juanpmachadob@gmail.com>
2022-05-11 15:13:21 -0500

    Changed deploy branch to gh-pages

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import { HashRouter as Router, Switch, Redirect } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
